58f4551d4d2f842e69a6268dc03fe0cdb7b137d4
gsultanshah <sultan.shah@plabesk.com>
2025-05-27 21:28:42 +0400

    Fix 13

--- src/app/api/constituency/block-codes/route.ts
+++ src/app/api/constituency/block-codes/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
 import Constituency from '@/models/Constituency';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     await connectDB();
